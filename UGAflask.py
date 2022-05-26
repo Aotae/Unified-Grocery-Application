5181c02b176c70cf8ed0cb672d242780b6c6d94c
Nathan Pang <nathan.ao.pang@gmail.com>
2022-05-26 16:23:49 -0700

    Got Ajax interaction working for the search.html template and the flask server

--- UGAflask.py
+++ UGAflask.py
@@ -21,6 +21,18 @@ def display_coupons():
 @app.route("/search")
 def display_search():
     return flask.render_template('search.html')
+# AJAX Handlers should return jsons instead of rendering pages
+@app.route("/_search")
+def search():
+    """
+    this should search the database for a product by keyword.
+    current iteration simply retuns the keywords given
+    """
+    keyword = request.args.get('keyword',type=str)
+    # print(f"keyword={keyword}")
+
+    return flask.jsonify({"keyword":keyword})
+
 
 if __name__ == "__main__":
     app.run(port =5000, host = "0.0.0.0")
