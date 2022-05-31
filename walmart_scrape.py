3ee2ec962d26864cc2da7ca2cc554edcd3f820ae
Jake Follett (藏田源貴） <jakefollett22@gmail.com>
2022-05-31 10:33:08 -0700

    walmart coupons for Eugene (2 stores)

---
+++ walmart_scrape.py
@@ -0,0 +1,44 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def main():
+    urls = ['https://www.coupons.com/coupons/?pid=25351&nid=10&zid=lm54&storezip=97408',
+           'https://www.coupons.com/coupons/?pid=25351&nid=10&zid=lm54&storezip=97402']
+
+    for url in urls:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        APP_COUPONSINC = soup.find_all('script')[3].text.strip()[21:-1]
+        data = json.loads(APP_COUPONSINC)
+        print(formatData(data))
+
+
+def getDictionaryFromJsonData(data):
+    bigDict = data.get('contextData')
+    for key in bigDict:
+        if key == "gallery":
+            mediumDict = bigDict.get('gallery')
+            for key_2 in mediumDict:
+                if key_2 == "podNote":
+                    smallDict = mediumDict.get('podCache')
+                    #print(smallDict)
+                    return smallDict
+    return None
+
+def formatData(bigData):
+    #print(couponDict)
+    couponDict = getDictionaryFromJsonData(bigData)
+    allData = []
+    for coupon in couponDict:
+        newDict = {}
+        productInforamation = couponDict.get(coupon)
+        #print(productInforamation["summary"])
+        newDict["summary"] = productInforamation["summary"]
+        newDict["details"] = productInforamation["details"]
+        newDict["brand"] = productInforamation["brand"]
+        allData.append(newDict)
+
+    return allData
+
+main()
